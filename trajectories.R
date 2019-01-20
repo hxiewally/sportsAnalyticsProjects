5c39b9cf280ac02a5f97dc0f475446fbbe40e376
Lingge Li <linggel@uci.edu>
2019-01-19 22:29:37 -0800

    Update trajectories.R

--- trajectories.R
+++ trajectories.R
@@ -1,5 +1,3 @@
-######################### Calculate receiver trajectories #####################################
-
 GetRouteFrames <- function(tracking.third.passing.df, this.playId) {
   # Get the route frames for one play during a game
   #
@@ -10,109 +8,46 @@ GetRouteFrames <- function(tracking.third.passing.df, this.playId) {
   # Returns:
   # data frame for the route frames or NULL if no forward pass is found
   tracking.this.play.df <- tracking.third.passing.df[tracking.third.passing.df$playId == this.playId, ]
-  # find the frame when the ball is snapped
-  is.snap <- tracking.this.play.df$event == 'ball_snap'
-  is.snap[is.na(is.snap)] <- FALSE
   # find the frame when the ball is thrown
   is.pass <- tracking.this.play.df$event == 'pass_forward'
   is.pass[is.na(is.pass)] <- FALSE  # get rid of NAs
-  #print(sum(is.pass))
-  if (sum(is.pass) == 0) {
-    print('No forward pass found in this play:')
+  is.snap <- tracking.this.play.df$event == 'ball_snap'
+  is.snap[is.na(is.snap)] <- FALSE
+  is.arrival <- tracking.this.play.df$event == 'pass_arrived'
+  is.arrival[is.na(is.arrival)] <- FALSE 
+  if (sum(is.pass) == 0 | sum(is.arrival) == 0) {
+    print('No valid pass in this play:')
     print(this.playId)
     return(NULL)
   } else {
     snap.frame.id <- tracking.this.play.df[is.snap, ]$frame.id[1]
-    pass.frame.id <- tracking.this.play.df[is.pass, ]$frame.id[1] - 1  # get frame id
-    print(this.playId)
+    arrival.frame.id <- tracking.this.play.df[is.arrival, ]$frame.id[1]
     tracking.frames.df <- tracking.this.play.df[tracking.this.play.df$frame.id >= snap.frame.id, ]
-    tracking.frames.df <- tracking.frames.df[tracking.frames.df$frame.id < pass.frame.id, ]
+    tracking.frames.df <- tracking.frames.df[tracking.frames.df$frame.id <= arrival.frame.id, ]
     return(tracking.frames.df)
   }
 }
 
 
-third.passing.plays.df <- plays.df %>% filter(gameId == game.tracking.df$gameId[1], down == 3, !is.na(PassLength))
-
-current.play.df <- game.tracking.df %>% filter(playId == 680)
-
-is.snap <- current.play.df$event == 'ball_snap'
-is.snap[is.na(is.snap)] <- FALSE
-is.pass <- current.play.df$event == 'pass_forward'
-is.pass[is.na(is.pass)] <- FALSE 
-snap.frame.id <- current.play.df[is.snap, ]$frame.id[1]
-pass.frame.id <- current.play.df[is.pass, ]$frame.id[1] - 1  # get frame id
-last.frame.id <- max(current.play.df$frame.id)
-if (pass.frame.id + 10 < last.frame.id) {
-  pass.frame.id <- pass.frame.id + 50  # get 1s after forward pass
-} else {
-  pass.frame.id <- last.frame.id
-}
-
-tracking.frames.df <- current.play.df[current.play.df$frame.id >= snap.frame.id, ]
-tracking.frames.df <- tracking.frames.df[tracking.frames.df$frame.id < pass.frame.id, ]
-
-
-CalculateRoutes <- function(route.frames, players.df) {
-  # Calculate route trajectories for all eligible receivers
-  #
-  # Args:
-  # route.frames: data frame for the passing time frame
-  # players.df: data frame of player information
-  #
-  # Returns:
-  # matrix of dim(t, 5)
-  
+GetReceiverRoutes <- function(route.frames, players.df) {
   route.frames.merged <- route.frames %>% inner_join(players.df)  # this gets rid of the ball
   qb <- route.frames.merged %>% filter(PositionAbbr == 'QB')
   route.frames.merged$offense <- route.frames.merged$team == qb$team[1]
   eligible.players <- route.frames.merged %>% filter(offense == TRUE & PositionAbbr != 'QB' & (jerseyNumber < 50 | jerseyNumber > 79))
-  
-  min.dist <- rep(0, 5)
-  
-  for (i in 1:dim(eligible.players)[1]) {
-    dist.vec <- rep(0, 11)
-    x1 <- eligible.players$x[i]
-    y1 <- eligible.players$y[i]
-    start.frame.id <- 
-    end.frame.id <- 
-    for (j in 1:11) {
-      
-    }
-    min.dist[i] <- min(dist.vec)
+  return(eligible.players)
+}
+
+
+CreateRouteFeatures <- function(eligible.players) {
+  route.features <- matrix(0, 5, 5)
+  for (i in 1:5) {
+    current.player <- eligible.players %>% filter(nflId == unique(eligible.players$nflId)[i])
+    current.features <- c(length(current.player$s), mean(current.player$s), sd(current.player$s),
+                          mean(current.player$dir), sd(current.player$dir))
+    route.features[i, ] <- current.features
   }
-  return(min.dist)
+  return(route.features)
 }
 
-route.frames <- tracking.frames.df
 
-route.frames.merged <- route.frames %>% inner_join(players.df)  # this gets rid of the ball
-qb <- route.frames.merged %>% filter(PositionAbbr == 'QB')
-route.frames.merged$offense <- route.frames.merged$team == qb$team[1]
-eligible.players <- route.frames.merged %>% filter(offense == TRUE & PositionAbbr != 'QB' & (jerseyNumber < 50 | jerseyNumber > 79))
-start.frame.id <- min(eligible.players$frame.id)
-end.frame.id <- max(eligible.players$frame.id)
-time.length <- end.frame.id - start.frame.id + 1
-trajectories <- matrix(0, time.length, 10)
-receiver.ids <- unique(eligible.players$nflId)
-for (t in start.frame.id:end.frame.id) {
-  for (i in 1:length(receiver.ids)) {
-    current.player <- eligible.players %>% filter(frame.id == t, nflId == receiver.ids[i])
-    trajectories[t - start.frame.id + 1, i * 2 - 1] <- current.player$x 
-    trajectories[t - start.frame.id + 1, i * 2] <- current.player$y
-  }
-}
-
-origin <- trajectories[1, ]
-for(j in 1:nrow(trajectories)) {
-  trajectories[j, ] <- trajectories[j, ] - origin
-}
-
-traj.long <- rbind(trajectories[, 1:2], trajectories[, 3:4], trajectories[, 5:6], 
-                   trajectories[, 7:8], trajectories[, 9:10])
-traj.df <- as.data.frame(traj.long)
-colnames(traj.df) <- c('x', 'y')
-
-traj.df$player <- rep(1:5, each=time.length)
-
-ggplot(traj.df) + geom_point(aes(x=x, y=y, colour=as.factor(player)))
+ggplot(eligible.players) + geom_point(aes(x=x, y=y, colour=as.factor(nflId)))
